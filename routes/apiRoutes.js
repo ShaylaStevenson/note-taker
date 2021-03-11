f0d46775dfc78c11031773af9ba3c654127bb040
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-03-11 13:06:07 -0800

    delete option added and working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -46,7 +46,30 @@ module.exports = (app) => {
         console.log(notesData);
     });
 
-    
+    // user wants to remove a note - DELETE
+    // example from Codota, author davidtran
+    app.delete('/api/notes/:id', (req, res) => {
+
+        // read the db.json file and save as rawData
+        const rawData = fs.readFileSync('./db/db.json');
+        // parse rawData to create usable content
+        const notesData = JSON.parse(rawData);
+
+        // id of the note being deleted
+        let noteId = req.params.id;
+
+        // find the index of the note with the matching id
+        let index = notesData.findIndex(note => note.id === noteId);
+
+        // remove the note from the array
+        notesData.splice(index, 1);
+
+        // rewrite the db.json file with the note removed
+        fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+
+        // display data
+        res.json(notesData)
+    });
 }
 
 
