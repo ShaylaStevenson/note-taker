79d96b38a5a89382d7ecbbcdcc66a92d61fbde0f
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-03-10 16:16:29 -0800

    ids added to notes in api.routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,44 @@
-// link to the data source
-const notesData = require('../db/db.json');
+// dependencies
+const fs = require('fs');
 
 // routing
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.join(notesData));
+    // read the db.json file and save as rawData
+    let rawData = fs.readFileSync('./db/db.json');
+
+    // parse rawData to create usable content
+    let notesData = JSON.parse(rawData);
     
+    //function to be called on to assign new key/value pair (id/index(+1))
+    function generateId(note, index) {
+        note.id = 1 + index;
+    };
+      
+    // apply the callback function to notesData using map, returning newly modified array
+    notesData.map(generateId);  
+    console.log(notesData);  
+    
+    
+    
+    app.get('/api/notes', (req, res) => res.join(notesData));
+
+
+
+    //post
+    // Create New Characters - takes in JSON input
+// app.post('/api/characters', (req, res) => {
+//     // req.body hosts is equal to the JSON post sent from the user
+//     // This works because of our body parsing middleware
+//     const newcharacter = req.body;
+  
+//     console.log(newcharacter);
+  
+//     // We then add the json the user sent to the character array
+//     characters.push(newcharacter);
+  
+//     // We then display the JSON to the users
+//     res.json(newcharacter);
+//   });
 }
 
 
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// dependencies
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+// setting up express
 const app = express();
 const PORT = 8080;
 
@@ -10,6 +12,7 @@ app.use(express.json());
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
+
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
 });
