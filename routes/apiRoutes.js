f29a1e94f5bf6d33e4284b974382fa3283166eb0
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-03-10 23:01:25 -0800

    restructured to try and connect to FE, no luck

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,36 @@
 // dependencies
 const fs = require('fs');
+const path = require('path');
 const uniqid = require('uniqid');
 
 // routing
 module.exports = (app) => {
-    // read the db.json file and save as rawData
-    let rawData = fs.readFileSync('./db/db.json');
-    
-    // parse rawData to create usable content
-    let notesData = JSON.parse(rawData);
-    
+
     // return as JSON and send to front end
-    app.get('/api/notes', (req, res) => res.json(notesData));
+    app.get('/api/notes', (req, res) => {
+
+        // read the db.json file and save as rawData
+        const rawData = fs.readFileSync('./db/db.json');
+        // parse rawData to create usable content
+        const notesData = JSON.parse(rawData);
+        
+        res.json(notesData);
+
+        console.log(notesData);
+        console.log(typeof notesData);
+    });
+    
     //console.log(notesData);
 
     // new note has been entered...
     app.post('/api/notes', (req, res) => {
-        
-        // create a newNote object to store input in
+
+        // read the db.json file and save as rawData
+        const rawData = fs.readFileSync('./db/db.json');
+        // parse rawData to create usable content
+        const notesData = JSON.parse(rawData);
+
+        //create a newNote object to store input in
         const newNote = {
             // use npm's Uniqid package to generate an id for the new note
             id: uniqid(),
@@ -25,15 +38,14 @@ module.exports = (app) => {
             text: req.body.text
         };
 
-        
         // push the new note to the array containing all notes
         notesData.push(newNote);
-
+        
         // rewrite the db.json file with the new note now added
         fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
 
         // display the new note
-        res.json(newNote);
+        res.json(true);
         
         console.log(notesData);
     });
--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const fs = require('fs');
 
 // setting up express
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // link to files in public, such as index.js and stylesheet
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 // paths to route files
 require('./routes/apiRoutes')(app);
